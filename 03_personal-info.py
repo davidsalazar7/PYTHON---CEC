33bae308f2a287aad937599dd42142535d8c8778
davidsalazar7 <59767939+davidsalazar7@users.noreply.github.com>
2020-01-17 14:19:32 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,7 @@
+FN=input("Nombre? ")
+LN=input("Apellido? ")
+ADD=input("Dirección? ")
+AGE=input("Edad? ")
+SPC=(" ")
+
+print("Hola"+SPC+FN+SPC+LN+SPC+"con dirección"+SPC+ADD+SPC+"y"+SPC+AGE+SPC+"años,"+SPC+"bienvenido")
---
+++ 04_if-vlan.py
@@ -0,0 +1,7 @@
+nativeVLAN=100
+dataVLAN=100
+
+if nativeVLAN == dataVLAN:
+    print("nativeVLAN y dataVLAN iguales.")
+else:
+    print("nativeVLAN y dataVLAN diferentes.")
